8c326abdb6fc817c294f571d6b8dceb81449bd10
Rexkit <PTZnikko@gmail.com>
2018-06-02 18:24:15 +0300

    initial commit :tada:

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, path.resolve(__dirname, 'app.js')],
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: path.resolve(__dirname, 'src/sass/main.scss'),
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
+            }
+        ]
+    }
+};
