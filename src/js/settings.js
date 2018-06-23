40c150c04ed996e64d43890bd9e2032496c7b15d
Rexkit <PTZnikko@gmail.com>
2018-06-23 14:49:51 +0300

    server url changed

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,2 +1,2 @@
-//export const serverURL = 'https://audioplayer-project.herokuapp.com/';
-export const serverURL = 'http://localhost:3000/';
+export const serverURL = 'https://audioplayer-project.herokuapp.com/';
+//export const serverURL = 'http://localhost:3000/';
